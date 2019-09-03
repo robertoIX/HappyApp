27bf4e8d8531be936c38036058fd766e57e5b9a5
Neatherblok <42801543+Neatherblok@users.noreply.github.com>
2019-09-03 14:06:29 +0200

    Add personal app

--- app/src/main/java/com/hz/happyapp/MainActivity.java
+++ app/src/main/java/com/hz/happyapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.hz.happyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.worksText);
+        button = (Button) findViewById(R.id.partyButton);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dansplaat);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mp.start();
+                textview.setText(R.string.happyTextClicked);
+            }
+        });
     }
 }
