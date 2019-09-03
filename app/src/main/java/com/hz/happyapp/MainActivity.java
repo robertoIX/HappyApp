4c72b0f8a9a11a044b0b3b3380f7b66bf2d5e40e
Zexrica <krij0046@hz.nl>
2019-09-03 12:39:26 +0200

    Add confetti button

--- app/src/main/java/com/hz/happyapp/MainActivity.java
+++ app/src/main/java/com/hz/happyapp/MainActivity.java
@@ -2,7 +2,16 @@ package com.hz.happyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+//import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button happyButton = findViewById(R.id.happyButton);
+        final TextView happyText = findViewById(R.id.happyText);
+        final nl.dionsegijn.konfetti.KonfettiView viewKonfetti = findViewById(R.id.viewKonfetti);
+
+        happyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                happyText.setText(R.string.happyTextClicked);
+
+                viewKonfetti.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.RECT, Shape.CIRCLE)
+                        .addSizes(new Size(12, 5))
+                        .setPosition(-50f, viewKonfetti.getWidth() + 50f, -50f, -50f)
+                        .streamFor(300, 5000L);
+            }
+        });
     }
 }
