ab3d23b8b5363eb13c3a2b37cb33b7a50492af88
robinJ <jong0259@hz.nl>
2019-09-03 13:22:03 +0200

    Add button and text on click to app

--- app/src/main/java/com/hz/happyapp/MainActivity.java
+++ app/src/main/java/com/hz/happyapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.hz.happyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMain = findViewById(R.id.btnMain);
+        final TextView text = findViewById(R.id.mainText);
+
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(R.string.sadlifeClicked);
+            }
+        });
     }
 }
