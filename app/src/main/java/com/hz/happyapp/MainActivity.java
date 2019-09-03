56216476a3ec6ef1d53547b71ce94db4e2dabc33
Tom de Bruijn <tomokp@users.noreply.github.com>
2019-09-03 11:37:04 +0200

    adds working button

--- app/src/main/java/com/hz/happyapp/MainActivity.java
+++ app/src/main/java/com/hz/happyapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.hz.happyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void onButton2Tap(View v) {
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "Ouch!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
+    }
+
