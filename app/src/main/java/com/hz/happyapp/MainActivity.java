359e292c22ac21ede3f31a3d355144577e692334
CaspervB2000 <broe0108@hz.nl>
2019-09-05 09:35:33 +0200

    Fixed MainActivity

--- app/src/main/java/com/hz/happyapp/MainActivity.java
+++ app/src/main/java/com/hz/happyapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.hz.happyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button test = findViewById(R.id.button2);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final KonfettiView viewKonfetti = findViewById(R.id.viewKonfetti);
+                viewKonfetti.build()
+                        .addColors(Color.BLUE, Color.GREEN, Color.MAGENTA, Color.YELLOW)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 4f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.RECT, Shape.CIRCLE)
+                        .addSizes(new Size(12, 5))
+                        .setPosition(-50f, viewKonfetti.getWidth() + 50f, -50f, -50f)
+                        .streamFor(300, 5000L);
+            }
+        });
     }
 }
+
